94b2d3e5a83324d764d2b4a775f68ef448c733ed
Marininha <50337599+maferrarez@users.noreply.github.com>
2022-03-18 18:26:16 -0300

    Update game.py

--- game.py
+++ game.py
@@ -41,17 +41,16 @@ u = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor
 user = choosenOne(u)
 
 ##getting the computer answer randomly
-print("Computer chose:")
 c = random.randint(0,2)
+print(f"Computer choice: \n{c}")
 computer = choosenOne(c)
 
 ##comparing the answers to get the result
 if((c == 0 and u == 1) or (u == 0 and c ==2) or (c == 1 and u == 2)):
-	print("You win.")
+    print("You win.")
 
 elif((u == 0 and c == 1) or (c == 0 and u ==2) or (u == 1 and c == 2)):
-	print("You lose.")
+    print("You lose.")
 
 elif(u == c):
-	print("It's a tie.")
-  
+    print("It's a tie.")
